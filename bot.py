a5d108b7ffbd130d4f2461614f293baf2d2e39fe
KKoble <124018323+KKoble@users.noreply.github.com>
2026-05-03 22:24:01 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,7 @@ from discord.ext import commands
 from discord import ui, app_commands
 import aiosqlite
 import os
-
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
